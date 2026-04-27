827053701c9da18acb4f8540c3e46e3abbaf62a0
iboror-art <bedeeb123456@gmail.com>
2026-04-27 05:35:06 +0300

    Update server.py

--- chat-server/server.py
+++ chat-server/server.py
@@ -56,4 +56,4 @@ def on_image(data):
     
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    socketio.run(app, host='0.0.0.0', port=port, async_mode='gevent')
+    socketio.run(app, host='0.0.0.0', port=port)
